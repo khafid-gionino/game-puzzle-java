7c428b8992a45fa72add495341b1774859fcc1b1
Ach.Khafid Salim <34251646+khafid75@users.noreply.github.com>
2019-07-07 04:24:04 +0700

    game puzzle

---
+++ src/pkg8/puzzle/Puzzle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg8.puzzle;
+
+/**
+ *
+ * @author user
+ */
+public class Puzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Gui8Puzzle gp = new Gui8Puzzle();
+        gp.looknfeel();
+        gp.setVisible(true);
+    }
+    
+}
